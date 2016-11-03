a2fa3116908c192e4e7207a34231385f3c8839f1
KyleTranquilli27 <KyleTranquilli27@gmail.com>
2016-11-03 00:14:17 -0400

    Update 1

---
+++ Main.js
@@ -0,0 +1,356 @@
+console.log("Hi");
+var block0 = document.getElementById("block0");
+var b0=block0.children;
+
+console.log("block0")
+
+block0.addEventListener("click",function(){
+    for(var i=0; i <b0.length; i++){
+        if(b0[i].tagName == "P"){
+            b0[i].style.display = 'block';
+        }
+    }
+    
+});
+var block1 = document.getElementById("block1");
+var b1=block1.children;
+
+console.log("block1")
+
+block1.addEventListener("click",function(){
+        
+    for(var i=0; i <b1.length; i++){
+        if(b1[i].tagName == "P"){
+            b1[i].style.display = 'block';
+        }
+    }
+    
+});
+var block2 = document.getElementById("block2");
+var b2=block2.children;
+
+console.log("block2")
+
+block2.addEventListener("click",function(){
+        
+    for(var i=0; i <b2.length; i++){
+        if(b2[i].tagName == "P"){
+            b2[i].style.display = 'block';
+        }
+    }
+});
+var block3 = document.getElementById("block3");
+var b3=block3.children;
+
+console.log("block3")
+
+block3.addEventListener("click",function(){
+        
+    for(var i=0; i <b3.length; i++){
+        if(b3[i].tagName == "P"){
+            b3[i].style.display = 'block';
+        }
+    }
+    
+});
+var block4 = document.getElementById("block4");
+var b4=block4.children;
+
+console.log("block4")
+
+block4.addEventListener("click",function(){
+        
+    for(var i=0; i <b4.length; i++){
+        if(b4[i].tagName == "P"){
+            b4[i].style.display = 'block';
+        }
+    }
+    
+});
+var block5 = document.getElementById("block5");
+var b5=block5.children;
+
+console.log("block5")
+
+block5.addEventListener("click",function(){
+        
+    for(var i=0; i <b5.length; i++){
+        if(b5[i].tagName == "P"){
+            b5[i].style.display = 'block';
+        }
+    }
+    
+});
+var block6 = document.getElementById("block6");
+var b6=block6.children;
+
+console.log("block6")
+
+block6.addEventListener("click",function(){
+        
+    for(var i=0; i <b6.length; i++){
+        if(b6[i].tagName == "P"){
+            b6[i].style.display = 'block';
+        }
+    }
+    
+});
+var block7 = document.getElementById("block7");
+var b7=block7.children;
+
+console.log("block7")
+
+block7.addEventListener("click",function(){
+        
+    for(var i=0; i <b7.length; i++){
+        if(b7[i].tagName == "P"){
+            b7[i].style.display = 'block';
+        }
+    }
+    
+});
+var block8 = document.getElementById("block8");
+var b8=block8.children;
+
+console.log("block8")
+
+block8.addEventListener("click",function(){
+        
+    for(var i=0; i <b8.length; i++){
+        if(b8[i].tagName == "P"){
+            b8[i].style.display = 'block';
+        }
+    }
+    
+});
+var block9 = document.getElementById("block9");
+var b9=block9.children;
+
+console.log("block9")
+
+block9.addEventListener("click",function(){
+        
+    for(var i=0; i <b9.length; i++){
+        if(b9[i].tagName == "P"){
+            b9[i].style.display = 'block';
+        }
+    }
+    
+});
+var block10 = document.getElementById("block10");
+var b10=block10.children;
+
+console.log("block10")
+
+block10.addEventListener("click",function(){
+        
+    for(var i=0; i <b10.length; i++){
+        if(b10[i].tagName == "P"){
+            b10[i].style.display = 'block';
+        }
+    }
+    
+});
+var block11 = document.getElementById("block11");
+var b11=block11.children;
+
+console.log("block11")
+
+block11.addEventListener("click",function(){
+        
+    for(var i=0; i <b11.length; i++){
+        if(b11[i].tagName == "P"){
+            b11[i].style.display = 'block';
+        }
+    }
+    
+});
+var block12 = document.getElementById("block12");
+var b12=block12.children;
+
+console.log("block12")
+
+block12.addEventListener("click",function(){
+        
+    for(var i=0; i <b12.length; i++){
+        if(b12[i].tagName == "P"){
+            b12[i].style.display = 'block';
+        }
+    }
+    
+});
+var block13 = document.getElementById("block13");
+var b13=block13.children;
+
+console.log("block13")
+
+block13.addEventListener("click",function(){
+        
+    for(var i=0; i <b13.length; i++){
+        if(b13[i].tagName == "P"){
+            b13[i].style.display = 'block';
+        }
+    }
+    
+});
+var block14 = document.getElementById("block14");
+var b14=block14.children;
+
+console.log("block14")
+
+block14.addEventListener("click",function(){
+        
+    for(var i=0; i <b14.length; i++){
+        if(b14[i].tagName == "P"){
+            b14[i].style.display = 'block';
+        }
+    }
+    
+});
+var block15 = document.getElementById("block15");
+var b15=block15.children;
+
+console.log("block15")
+
+block15.addEventListener("click",function(){
+        
+    for(var i=0; i <b15.length; i++){
+        if(b15[i].tagName == "P"){
+            b15[i].style.display = 'block';
+        }
+    }
+    
+});
+var block16 = document.getElementById("block16");
+var b16=block16.children;
+
+console.log("block16")
+
+block16.addEventListener("click",function(){
+        
+    for(var i=0; i <b16.length; i++){
+        if(b16[i].tagName == "P"){
+            b16[i].style.display = 'block';
+        }
+    }
+    
+});
+var block17 = document.getElementById("block17");
+var b17=block17.children;
+
+console.log("block17")
+
+block17.addEventListener("click",function(){
+        
+    for(var i=0; i <b17.length; i++){
+        if(b17[i].tagName == "P"){
+            b17[i].style.display = 'block';
+        }
+    }
+    
+});
+var block18 = document.getElementById("block18");
+var b18=block18.children;
+
+console.log("block18")
+
+block18.addEventListener("click",function(){
+        
+    for(var i=0; i <b18.length; i++){
+        if(b18[i].tagName == "P"){
+            b18[i].style.display = 'block';
+        }
+    }
+    
+});
+var block19 = document.getElementById("block19");
+var b19=block19.children;
+
+console.log("block19")
+
+block19.addEventListener("click",function(){
+        
+    for(var i=0; i <b19.length; i++){
+        if(b19[i].tagName == "P"){
+            b19[i].style.display = 'block';
+        }
+    }
+    
+});
+var block20 = document.getElementById("block20");
+var b20=block20.children;
+
+console.log("block20")
+
+block20.addEventListener("click",function(){
+        
+    for(var i=0; i <b20.length; i++){
+        if(b20[i].tagName == "P"){
+            b20[i].style.display = 'block';
+        }
+    }
+    
+});
+var block21 = document.getElementById("block21");
+var b21=block21.children;
+
+console.log("block21")
+
+block21.addEventListener("click",function(){
+        
+    for(var i=0; i <b21.length; i++){
+        if(b21[i].tagName == "P"){
+            b21[i].style.display = 'block';
+        }
+    }
+    
+});
+var block22 = document.getElementById("block22");
+var b22=block22.children;
+
+console.log("block22")
+
+block22.addEventListener("click",function(){
+        
+    for(var i=0; i <b22.length; i++){
+        if(b22[i].tagName == "P"){
+            b22[i].style.display = 'block';
+        }
+    }
+    
+});
+var block23 = document.getElementById("block23");
+var b23=block23.children;
+
+console.log("block23")
+
+block23.addEventListener("click",function(){
+        
+    for(var i=0; i <b23.length; i++){
+        if(b23[i].tagName == "P"){
+            b23[i].style.display = 'block';
+        }
+    }
+    
+});
+var block24 = document.getElementById("block24");
+var b24=block24.children;
+
+console.log("block24")
+
+block24.addEventListener("click",function(){
+        
+    for(var i=0; i <b24.length; i++){
+        if(b24[i].tagName == "P"){
+            b24[i].style.display = 'block';
+        }
+    }
+    
+});
+
+
+
+ 
+ 
+
+
